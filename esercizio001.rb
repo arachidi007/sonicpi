94ebe0c83f08cc7fec549074350ef05aa712cf0e
arachidi007 <45451907+arachidi007@users.noreply.github.com>
2018-12-01 10:22:26 +0100

    Update esercizio001.rb

--- esercizio001.rb
+++ esercizio001.rb
@@ -1,11 +1,35 @@
+# AIRATO 1.0
+
 use_bpm 30
+
 live_loop :kikdrum do
   sample :bd_haus
-  sleep 0.25
-end
-
-live_loop :hihats do
-  sync :kikdrum
   sample :drum_cymbal_closed
   sleep 0.25
 end
+
+live_loop :piano  do
+  sample :ambi_piano
+  sleep 0.25
+  sample :ambi_piano
+  sleep 1
+  sample :ambi_piano
+  sleep 0.20
+  sample :ambi_piano
+  sleep 0.20
+end
+
+# QUESTI LI STAI LEGGENDO SOLO ALL'AVVIO
+# SENZA LOOP END NON SI RIPETONO
+sample  :elec_blip2
+sleep 0.25
+sample :elec_blip2
+sleep 0.25
+sample :elec_blip2
+sleep 0.25
+sample :elec_blip2
+sleep 0.25
+sample :elec_blip2
+sleep 0.25
+sample :elec_blip2
+sleep 0.25
