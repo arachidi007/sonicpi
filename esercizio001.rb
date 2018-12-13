4e25e6e94f1cdbb84c84efad563cca2f53e1f1ca
arachidi007 <45451907+arachidi007@users.noreply.github.com>
2018-12-13 14:48:05 +0100

    Update esercizio001.rb

--- esercizio001.rb
+++ esercizio001.rb
@@ -19,17 +19,29 @@ live_loop :piano  do
   sleep 0.20
 end
 
-# QUESTI LI STAI LEGGENDO SOLO ALL'AVVIO
-# SENZA LOOP END NON SI RIPETONO
-sample  :elec_blip2
-sleep 0.25
-sample :elec_blip2
-sleep 0.25
-sample :elec_blip2
-sleep 0.25
-sample :elec_blip2
-sleep 0.25
-sample :elec_blip2
-sleep 0.25
-sample :elec_blip2
-sleep 0.25
+live_loop :ciccio do
+  with_fx :compressor do
+    play 50
+  end
+  sample  :elec_blip2
+  sleep 1
+  sample :elec_blip2
+  sleep 0.25
+  sample :elec_blip2
+  sleep 1
+  sample :elec_blip2
+  sleep 0.25
+  sample :elec_blip2
+  sleep 1
+  sample :elec_blip2
+  sleep 0.25
+end
+
+live_loop :chitarra do
+  sample :guit_harmonics
+  sleep 0.25
+end
+live_loop :basso do
+  sample :bass_hard_c, amp: 0.3
+  sleep 0.25
+end
